2afdf2f58a98bf3075379cd924f6a6a472305a01
dianalejandra <diana.abate@gmail.com>
2024-07-03 17:43:38 -0300

    new exercises with tuples

--- operations-with-tuples/access-elem-in-tuple.py
+++ operations-with-tuples/access-elem-in-tuple.py
@@ -1,4 +1,5 @@
-# Access element in tuple_test = ("Orange,[10,20,30],(5,15,25)")
+# Access element 20 in tuple_test = ("Orange,[10,20,30],(5,15,25)")
 
-tuple_test = "Orange",[10,20,30],(5,15,25)
-print(tuple_test(1[1]))
+tuple_test = ("Orange",[10,20,30],(5,15,25))
+
+print(tuple_test[1][1])
