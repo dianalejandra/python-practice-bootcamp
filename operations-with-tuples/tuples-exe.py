850fbd3685345f4522daf5f096fb5110a4765ec2
dianalejandra <diana.abate@gmail.com>
2024-07-03 17:49:01 -0300

    new exercises with tuples

--- operations-with-tuples/tuples-exe.py
+++ operations-with-tuples/tuples-exe.py
@@ -1,12 +1,12 @@
 # Create a funtion that receives a list and returns a tuple with the even elements in that list
-def list_converter():
-    my_list = [1,2,3,4,5,6,7,8,9]
-    for number in my_list:
-        even_tuple = ()
-        if(number % 2 == 0):
-            even_tuple = number
-            print(number)
-            return number
-        
-    
-    
+
+def get_even_numbers_from_list(numList):
+    new_list = []    
+    for pair in numList:
+        if (pair % 2 == 0):
+            new_list.append(pair)
+    return tuple(new_list)
+            
+numList = [1,2,3,4,5,6,7,8,9]
+num_tuple = get_even_numbers_from_list(numList)
+print(num_tuple)
